aa4ad49e1ce3b79f96b67de9add3d2937a5cb98a
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-10-27 00:50:33 +0530

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,5 +1,15 @@
-SELECT customer_id
-	,SUM(price)
-FROM sales s
-LEFT JOIN menu m ON s.product_id = m.product_id
-GROUP BY 1
+
+
+    SELECT customer_id
+    	,SUM(price) as total_price
+    FROM dannys_diner.sales s
+    LEFT JOIN dannys_diner.menu m ON s.product_id = m.product_id
+    GROUP BY 1;
+
+| customer_id | sum |
+| ----------- | --- |
+| B           | 74  |
+| C           | 36  |
+| A           | 76  |
+
+---
